c72afda00dd0e69c91bbbd2cf058d5eb5775d8db
ZacGamble <zacrgamble@gmail.com>
2022-04-22 17:42:27 -0600

    'pre-account model'

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -4,7 +4,13 @@ export const CommentSchema = new Schema({
   creatorName: { type: String, required: true },
   body: { type: String, required: true },
   creatorId: { type: String, required: true },
-  postId: { type: Schema.Types.ObjectId }
+  postId: { type: Schema.Types.ObjectId, required: true }
 },
 { timestamps: true, toJSON: { virtuals: true } }
 )
+CommentSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
-// creatorId: { type: Schema.Types.ObjectId, ref: 'Profile', required: true }, REVIEW
 export const PostSchema = new Schema(
   {
     creatorName: { type: String, required: true },
     body: { type: String, required: true },
-    creatorId: { type: String, required: true },
+    // creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },TODO
     score: { type: Number },
     datePosted: { type: Date },
     title: { type: String }
@@ -13,9 +12,9 @@ export const PostSchema = new Schema(
   { timestamps: true, toJSON: { virtuals: true } }
 )
 
-// PostSchema.virtual('creator', { // change virtual tags? REVIEW
-//   localField: 'creatorId',
-//   foreignField: '_id',
-//   justOne: true,
-//   ref: 'Profile'
-// })
+PostSchema.virtual('creator', { // change virtual tags? REVIEW
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
